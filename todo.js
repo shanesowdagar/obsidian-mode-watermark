54f4c29717decbc898f3dc4cd4dd4a801295090f
Shane 😁 <strainedhalo1@gmail.com>
2026-03-05 19:50:25 +0400

    Add todos

--- todo.js
+++ todo.js
@@ -1,3 +1,3 @@
-// TODO Create a beautiful settings page for the plugin
+// FIXME Make the settings screen more aligned and make sense
 // TODO Fix all TSC errors to make the project simple
 // TODO Merge and release the plugin
