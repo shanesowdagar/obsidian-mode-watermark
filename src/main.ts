a73a68abb5d96155491e58b2d1a99635c7d59373
Shane 😁 <strainedhalo1@gmail.com>
2026-03-07 08:28:43 +0400

    Fix linting issues

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Plugin } from "obsidian";
 import { ModeWatermarkSettingTab } from "settings";
 
 export default class ModeWatermarkPlugin extends Plugin {
-	async onload() {
+	onload() {
 		this.addSettingTab(new ModeWatermarkSettingTab(this.app, this));
 	}
 }
